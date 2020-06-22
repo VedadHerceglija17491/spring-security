1478572948e1792cb4d9daab041913a453c2041c
VedadHerceglija17491 <vherceglij1@etf.unsa.ba>
2020-06-22 15:08:50 +0200

    Test API and Disable CSRF

--- src/main/java/com/example/demo/student/StudentManagementController.java
+++ src/main/java/com/example/demo/student/StudentManagementController.java
@@ -23,15 +23,18 @@ public class StudentManagementController {
     @PostMapping
     public void registerNewStudent(@RequestBody Student student) {
         System.out.println(student);
+        System.out.println("registerNewStudent");
     }
 
     @DeleteMapping(path = "{studentId}")
     public void deleteStudent(@PathVariable("studentId") Integer studentId) {
         System.out.println(studentId);
+        System.out.println("deleteStudent");
     }
 
     @PutMapping(path = "{studentId}")
     public void updateStudent(@PathVariable("studentId") Integer studentId, @RequestBody Student student) {
         System.out.println(String.format("%s %s", student, student));
+        System.out.println("updateStudent");
     }
 }
