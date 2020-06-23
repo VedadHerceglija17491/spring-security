7e1c0a99c6538956cfaec39bd137b675ca636b16
VedadHerceglija17491 <vherceglij1@etf.unsa.ba>
2020-06-23 12:31:20 +0200

    Setting authorities to users and fixErrors

--- src/main/java/com/example/demo/security/ApplicationSecurityConfig.java
+++ src/main/java/com/example/demo/security/ApplicationSecurityConfig.java
@@ -51,21 +51,24 @@ public class ApplicationSecurityConfig extends WebSecurityConfigurerAdapter {
         UserDetails annaSmithUser  = User.builder()
                 .username("annasmith")
                 .password(passwordEncoder.encode("password"))
-                .roles(STUDENT.name())
+             //   .roles(STUDENT.name())
+                .authorities(STUDENT.getGrantedAuthorities())
                 .build();
         UserDetails lindaUser = User.builder()
                 .username("linda")
                 .password(passwordEncoder.encode("password123"))
-                .roles(ADMIN.name())
+               // .roles(ADMIN.name())
+                .authorities(ADMIN.getGrantedAuthorities())
                 .build();
 
         UserDetails tomUser = User.builder()
                 .username("tom")
                 .password(passwordEncoder.encode("password123"))
-                .roles(ADMIN.name())
+                .authorities(ADMINTRAINEE.getGrantedAuthorities())
+                //.roles(ADMIN.name())
                 .build();
         return new InMemoryUserDetailsManager(
-            annaSmithUser,
+                annaSmithUser,
                 lindaUser,
                 tomUser
         );
